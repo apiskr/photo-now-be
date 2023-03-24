560b522b9a4ee9e9fe4ad9f4899be93592ed867d
Jchan <gkswocks123@gmail.com>
2023-03-24 23:12:46 +0900

    ✨ Feat : PhotoEntity 칼럼 추가

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,20 +1,23 @@
 import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
+import { PhotoEntity } from 'src/photo/photo.entity';
 
-// [Todo] databaseProviders 타입 설정하기
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
-    useFactory: async (configService: ConfigService) => {
+    useFactory: async () => {
+      console.log(__dirname);
       const dataSource = new DataSource({
         type: 'postgres',
-        host: configService.get<string>('DATABASE_HOST'),
+        host: process.env.DATABASE_HOST,
         port: 5432,
-        username: configService.get<string>('DATABASE_USERNAME'),
-        password: configService.get<string>('DATABASE_PASSWORD'),
-        database: configService.get<string>('DATABASE_DATABASE'),
-        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DATABASE,
+        entities: [PhotoEntity], // [Error] 밑에 코드 안 먹음
+        // entities: [__dirname + '/../**/*.entity.{js,ts}'],
         synchronize: true,
+        dropSchema: true, // [Todo] 이 설정으로 재실행할 때마다 스키마 날리는 중
       });
       return dataSource.initialize();
     },
